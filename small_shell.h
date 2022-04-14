d582b1e4aa4e27127e7462045db837acafa85185
nofar.av <nofar.av@campus.technion.ac.il>
2022-04-14 20:36:32 +0300

    start on small shell

---
+++ small_shell.h
@@ -0,0 +1,24 @@
+#ifndef SMALL_SHELL_H_
+#define SMALL_SHELL_H_
+
+
+class SmallShell {
+ private:
+  // TODO: Add your data members
+  SmallShell();
+ public:
+  Command *CreateCommand(const char* cmd_line);
+  SmallShell(SmallShell const&)      = delete; // disable copy ctor
+  void operator=(SmallShell const&)  = delete; // disable = operator
+  static SmallShell& getInstance() // make SmallShell singleton
+  {
+    static SmallShell instance; // Guaranteed to be destroyed.
+    // Instantiated on first use.
+    return instance;
+  }
+  ~SmallShell();
+  void executeCommand(const char* cmd_line);
+  // TODO: add extra methods as needed
+};
+
+#endif //SMALL_SHELL_H_

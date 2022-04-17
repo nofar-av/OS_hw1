cbc555e04c6b9445cc9b5976c22717bd231a7980
nofar.av <nofar.av@campus.technion.ac.il>
2022-04-17 16:49:55 +0300

    change exceptions layout

--- exceptions.cpp
+++ exceptions.cpp
@@ -1,44 +1,50 @@
 #include "exceptions.h"
-
+#include <errno.h>
 using namespace std;
 
 
-const char* SmashException::what() const noexcept
+const char* BuiltInException::what() const noexcept
 {
     return error_message.c_str();
 }
-SmashException::SmashException(const string cmd_name)
+BuiltInException::BuiltInException(const string cmd_name)
 { 
     this->error_message = "smash error:" + cmd_name + ":" + this->error_message + "\n"; 
+}
+SyscallException::SyscallException(string syscall_name) {
+        this->error_message = "smash error: " + syscall_name + " failed";
 }
 InvalidlArguments::InvalidlArguments(const string cmd_name) 
 {
     this->error_message = "invalid arguments";
-    SmashException(cmd_name);
+    BuiltInException::BuiltInException(cmd_name);
 }
 JobIdDoesntExist::JobIdDoesntExist(const string cmd_name, int job_id)
 {
     this->error_message = "job-id" + job_id + "does not exist";
-    SmashException::SmashException(cmd_name);
+    BuiltInException::BuiltInException(cmd_name);
+}
 }
 JobAlreadyRunBG::JobAlreadyRunBG(const string cmd_name, int job_id)
 {
     this->error_message = "job-id" + job_id + "is already running in the background";
-    SmashException::SmashException(cmd_name);
+    BuiltInException::BuiltInException(cmd_name);
+}
 }
 OldpwdNotSet::OldpwdNotSet(const string cmd_name)
 {
     this->error_message = "OLDPWD not set";
-    SmashException::SmashException(cmd_name);
+    BuiltInException::BuiltInException(cmd_name);
 }
 NoStoppedJobs::NoStoppedJobs(const string cmd_name)
 {
     this->error_message = "there is no stopped jobs to resume";
-    SmashException::SmashException(cmd_name);
+    BuiltInException::BuiltInException(cmd_name);
 }
 
 TooManyArgs::TooManyArgs(const string cmd_name)
 {
     this->error_message = "too many arguments";
-    SmashException::SmashException(cmd_name);
+    BuiltInException::BuiltInException(cmd_name);
+}
 }
--- exceptions.h
+++ exceptions.h
@@ -12,48 +12,57 @@ using namespace std;
 
 
 class SmashException : public std::exception
-{
+{};
+class BuiltInException : public SmashException {
 protected:
     string error_message;
 public:
-    SmashException(const string cmd_name);
-    SmashException()  = default;
-    ~SmashException() = default;
+    BuiltInException(const string cmd_name);
+    BuiltInException()  = default;
+    ~BuiltInException() = default;
     virtual const char* what() const noexcept override;
 };
-class InvalidlArguments : public SmashException
+class SyscallException : public SmashException {
+  private:
+    string error_message;
+
+  public:
+    SyscallException(string syscall_name);
+    const char *what() const noexcept override { return error_message.c_str(); }
+};
+class InvalidlArguments : public BuiltInException
 {
 public:
     InvalidlArguments(const string cmd_name);
     ~InvalidlArguments() = default;
 };
-class JobIdDoesntExist : public SmashException
+class JobIdDoesntExist : public BuiltInException
 {
 public:
     JobIdDoesntExist(const string cmd_name, int job_id);
     ~JobIdDoesntExist() = default;
 };
-class JobAlreadyRunBG : public SmashException
+class JobAlreadyRunBG : public BuiltInException
 {
 public:
     JobAlreadyRunBG(const string cmd_name, int job_id);
     ~JobAlreadyRunBG() = default;
 };
 
-class OldpwdNotSet : public SmashException
+class OldpwdNotSet : public BuiltInException
 {
 public:
     OldpwdNotSet(const string cmd_name);
     ~OldpwdNotSet() = default;
 };
 
-class NoStoppedJobs : public SmashException
+class NoStoppedJobs : public BuiltInException
 {
 public:
     NoStoppedJobs(const string cmd_name);
     ~NoStoppedJobs() = default;
 };
-class TooManyArgs : public SmashException
+class TooManyArgs : public BuiltInException
 {
 public:
     TooManyArgs(const string cmd_name);
